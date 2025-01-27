0c5678458046e09951bf6be402f1e514e1acb435
Amina02-KC <aminagibadullina64@gmail.com>
2025-01-27 22:00:23 +0300

    Delete 24.12.2025 directory

--- 24.12.2025/Gibadullina_task6/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module ru.gibadullina.gibadullina_task6 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens ru.gibadullina.gibadullina_task6 to javafx.fxml;
-    exports ru.gibadullina.gibadullina_task6;
-}

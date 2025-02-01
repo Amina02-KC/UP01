3a3e761a0e01bc0a597254fe5285890c949c1cb3
Amina02-KC <aminagibadullina64@gmail.com>
2025-02-01 23:31:49 +0300

    Delete homework directory

--- homework/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module ru.gibadullina.homework {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens ru.gibadullina.homework to javafx.fxml;
-    exports ru.gibadullina.homework;
-}
